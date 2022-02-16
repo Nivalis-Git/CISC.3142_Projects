a27ed469596bd045ff0a59ee5396607794c6b7f0
Simon <s.n.shterenberg@gmail.com>
2022-02-16 13:45:44 -0500

    Fixed parser's line skipping error
    Adjusted the loop that reads data.csv, no longer reads past end of file

--- lab1/data_parser.cpp
+++ lab1/data_parser.cpp
@@ -1,12 +1,17 @@
 // This file shows basic functions of reading in a CSV file using C++98
-// Please add your name to your code! 
+// Please add your name to your code!
 
-/* Homework Objective: fix this program to match the requirements listed: 
+/* Homework Objective: fix this program to match the requirements listed.
   1. successfully reads all lines of the input file
-  2. calculate the average price per brand, average price per category 
+  2. calculate the average price per brand, average price per category
   3. writes to an output file
   4. for each unique year, list the count of skus and also print out the skus as a list i.e. 2000 (3): 111, 211, 311 and make a new line per year.
-All programs must be able to compile in C++98 standard (the default version on Linux Machine). That means you cannot use the new features of C++11! :(
+  All programs must be able to compile in C++98 standard (the default version on Linux Machine). That means you cannot use the new features of C++11! :(
+*/
+
+/* Student Information
+  Shterenberg, Simon
+  3142 CISC, EW6
 */
 
 #include <iostream>
@@ -15,68 +20,63 @@ All programs must be able to compile in C++98 standard (the default version on L
 #include <vector>
 #include <fstream>
 
-using namespace std; 
+using namespace std;
 
 int main() {
 
   // define variables
-  string sku, brand, category, year, price; 
-  vector<int>vSKU;
-  vector<string>vBrand;
-  vector<string>vCategory;
-  vector<int>vYear;
-  vector<float>vPrice;
+  string sku, brand, category, year, price;
+  vector<int> vSKU;
+  vector<string> vBrand;
+  vector<string> vCategory;
+  vector<int> vYear;
+  vector<float> vPrice;
 
   ifstream in_stream;
-  in_stream.open("data.csv"); //opening the file.
+  in_stream.open("data.csv"); // opening the file
 
-  if (!in_stream.fail()) { //if the file is open
+  if (!in_stream.fail()) { // if the file is open, execute
+    string header;
+    getline(in_stream, header);
 
-    string line;
-
-    while (getline(in_stream, line)) { //while the end of file is NOT reached
-
-     // Fields: sku,brand,category,year,price
-
-     getline(in_stream, sku, ',');
-     stringstream ssku(sku); 
-     int iSKU = 0;
-     ssku >> iSKU;
-     vSKU.push_back(iSKU);
-
-     getline(in_stream, brand, ',');
-     vBrand.push_back(brand);
-
-     getline(in_stream, category, ',');
-     vCategory.push_back(category);
-
-     getline(in_stream, year, ','); 
-     stringstream syear(year);
-     int iYear;
-     syear >> iYear;
-     vYear.push_back(iYear);
-
-     getline(in_stream, price, '\n'); 
-     stringstream sprice(price);
-     float fPrice;
-     sprice >> fPrice;
-     vPrice.push_back(fPrice);
-     
+    while (in_stream.peek() != EOF) { // while eof is NOT reached (avoids reading past eof)
+      // fields: sku, brand, category, year, price
+      
+      getline(in_stream, sku, ',');
+      stringstream ssku(sku);
+      int iSKU = 0;
+      ssku >> iSKU;
+      vSKU.push_back(iSKU);
+      
+      getline(in_stream, brand, ',');
+      vBrand.push_back(brand);
+      
+      getline(in_stream, category, ',');
+      vCategory.push_back(category);
+      
+      getline(in_stream, year, ',');
+      stringstream syear(year);
+      int iYear;
+      syear >> iYear;
+      vYear.push_back(iYear);
+      
+      getline(in_stream, price, '\n');
+      stringstream sprice(price);
+      float fPrice;
+      sprice >> fPrice;
+      vPrice.push_back(fPrice);
     }
-
-    in_stream.close(); //closing the file cout << "Number of entries: " << i-1 << endl;
-
-   } else {
-    cout << "Unable to open file"; 
-   }
- 
-  //output values 
-  cout << "SKU" << "\t" << "Brand" << "\t" << "Year" << endl; 
-
+    
+    in_stream.close(); // closing the file cout << "Number of entries: " << i-1 << endl;
+  } else {
+      cout << "Unable to open file";
+  }
+  
+  cout << "SKU" << "\t" << "Brand" << "\t" << "Year" << endl;
+  
   for (int j = 0; j < vSKU.size(); j++) {
     cout << vSKU[j] << "\t" << vBrand[j] << "\t" << vYear[j] << endl;
   }
-
+  
   cout << endl;
-
 }
