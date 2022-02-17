1ca33634093c9f1d98024b3a630c7ed39a1520d6
Simon <s.n.shterenberg@gmail.com>
2022-02-17 16:06:47 -0500

    Switched to using three 1-D vectors to calculate average prices
    Plan to replace the std::find algorithm by using a hash_map for efficient filling of <owners>
    
    Plan to use a vector of pairs to replace <sumofPrices> and <countOfPrices>

--- lab1/data_parser.cpp
+++ lab1/data_parser.cpp
@@ -23,6 +23,8 @@
 
 using namespace std;
 
+void pairing_oneToMany(vector<string>& vOne, vector<string>& vMany, vector<stringstream>& vOwners);
+
 int main() {
 
   // define variables
@@ -32,15 +34,17 @@ int main() {
   vector<string> vCategory;
   vector<int> vYear;
   vector<float> vPrice;
-
+  
+  // opening the file
   ifstream in_stream;
-  in_stream.open("data.csv"); // opening the file
-
-  if (!in_stream.fail()) { // if the file is open, execute
+  in_stream.open("data.csv");
+  
+  // if the file is open, execute
+  if (!in_stream.fail()) {
     string header;
     getline(in_stream, header);
 
-    while (in_stream.peek() != EOF) { // while eof is NOT reached (avoids reading past eof)
+    while (in_stream.peek() != EOF) {  // while eof is NOT reached (avoids reading past eof)
       // fields: sku, brand, category, year, price
       
       getline(in_stream, sku, ',');
@@ -68,44 +72,47 @@ int main() {
       vPrice.push_back(fPrice);
     }
     
-    in_stream.close(); // closing the file cout << "Number of entries: " << i-1 << endl;
+    in_stream.close();  // closing the file cout << "Number of entries: " << i-1 << endl;
   } else {
       cout << "Unable to open file";
   }
   
-  // output values to text file
+  // output series of "SKU Brand Year" to text file
   ofstream out_stream("data_analysis.txt");
   
   out_stream << "SKU" << "\t" << "Brand" << "\t" << "Year" << endl;
   for (int j = 0; j < vSKU.size(); j++) {
     out_stream << vSKU[j] << "\t" << vBrand[j] << "\t" << vYear[j] << endl;
   }
-  out_stream << endl;
+  out_stream << endl << endl;
   
   // Calculating the average price per brand and per category
-  vector<string> owners; // tracks the unique owner values
-  vector< vector<float> > prices;
+  vector<string> owners;      // tracks the unique owner values
+  vector<float> sumOfPrices;  // tracks the sum of prices associated with an owner
+  vector<int> countOfPrices;  // tracks the number of prices associated with an owner
+  // the above vectors match their elements with the same indices, i.e. a[i]<-->b[i]<-->c[i]
+  
+  vector<string>::iterator it;
   for (int i = 0; i < vBrand.size(); i++) {
-    vector<string>::iterator it = find(owners.begin(), owners.end(), vBrand[i]);
+    it = find(owners.begin(), owners.end(), vBrand[i]);
     if (it == owners.end()) {
       owners.push_back(vBrand[i]);
-      prices.push_back(vector<float>());
-      prices[prices.size()-1].push_back(vPrice[i]);
+      sumOfPrices.push_back(vPrice[i]);
+      countOfPrices.push_back(1);
     } else {
-      prices[it-owners.begin()].push_back(vPrice[i]);
+      sumOfPrices[it-owners.begin()] += vPrice[i];
+      countOfPrices[it-owners.begin()]++;
     }
   }
   
+  out_stream << "Brand" << "\t" << "Average_Price" << endl;
+  char* average;  // buffer for correctly formatted average
   for (int i = 0; i < owners.size(); i++) {
-    cout << owners[i] << endl;
-  }
-  cout << endl;
-  for (int i = 0; i < prices.size(); i++) {
-    for (int j = 0; j < prices[i].size(); j++) {
-      cout << prices[i][j] << " ";
-    }
-    cout << endl;
+    sprintf(average, "%8.2f", sumOfPrices[i]/countOfPrices[i]);
+    out_stream << owners[i] << "\t" << average << endl;
   }
   
   out_stream.close();
+  
+  cout << endl;
 }
