fdda7c65f297322131a7cb368cf20a30c1ad4036
Nivalis-Git <s.n.shterenberg@gmail.com>
2022-03-22 21:22:23 -0400

    Winnowed the code a bit
    Added an invariant condition for the for loop printing SKU, Brand, Year

--- lab1_src/data_parser.cpp
+++ lab1_src/data_parser.cpp
@@ -1,17 +1,6 @@
-// This file shows basic functions of reading in a CSV file using C++98
-// Please add your name to your code!
-
-/* Homework Objective: fix this program to match the requirements listed.
-  1. successfully reads all lines of the input file
-  2. calculate the average price per brand, average price per category
-  3. writes to an output file
-  4. for each unique year, list the count of skus and also print out the skus as a list i.e. 2000 (3): 111, 211, 311 and make a new line per year.
-  All programs must be able to compile in C++98 standard (the default version on Linux Machine). That means you cannot use the new features of C++11! :(
-*/
-
 /* Student Information
   Shterenberg, Simon
-  3142 CISC, EW6
+  Project 1
 */
 
 #include "data_parser.h"
@@ -23,19 +12,14 @@ int main()
   float num = 5.569483303;
   printf("Greetings. My name--%s.\n%.2f\n", name.c_str(), num);
 
-  // I. Printing SKU, Brand, and Year for each record
-  
+// I. Printing SKU, Brand, and Year for each record
   // define variables
   string sku, brand, category, year, price;
-  vector<string> vSKU;
-  vector<string> vBrand;
-  vector<string> vCategory;
-  vector<string> vYear;
+  vector<string> vSKU, vBrand, vCategory, vYear;
   vector<float> vPrice;
   
   // opening the file
-  ifstream in_stream;
-  in_stream.open("data.csv");
+  ifstream in_stream("data.csv");
   
   // if the file is open, execute
   if (!in_stream.fail())
@@ -48,10 +32,7 @@ int main()
       // fields: sku, brand, category, year, price
       
       getline(in_stream, sku, ',');
-      stringstream ssku(sku);
-      string iSKU;
-      ssku >> iSKU;
-      vSKU.push_back(iSKU);
+      vSKU.push_back(sku);
       
       getline(in_stream, brand, ',');
       vBrand.push_back(brand);
@@ -60,10 +41,7 @@ int main()
       vCategory.push_back(category);
       
       getline(in_stream, year, ',');
-      stringstream syear(year);
-      string iYear;
-      syear >> iYear;
-      vYear.push_back(iYear);
+      vYear.push_back(year);
       
       getline(in_stream, price, '\n');
       stringstream sprice(price);
@@ -72,7 +50,7 @@ int main()
       vPrice.push_back(fPrice);
     }
     
-    in_stream.close();  // closing the file cout << "Number of entries: " << i-1 << endl;
+    in_stream.close();
   } else {
       cout << "Unable to open file";
   }
@@ -81,16 +59,18 @@ int main()
   ofstream out_stream("data_analysis.txt");
   
   out_stream << "SKU" << "\t" << "Brand" << "\t" << "Year" << endl;
-  for (int j = 0; j < vSKU.size(); j++)
+  if ( vSKU.size() == vBrand.size() && vSKU.size() == vYear.size() )
   {
-    out_stream << vSKU[j] << "\t" << vBrand[j] << "\t" << vYear[j] << endl;
+  	for (int j = 0; j < vSKU.size(); j++)
+  	{
+    	out_stream << vSKU[j] << "\t" << vBrand[j] << "\t" << vYear[j] << endl;
+  	}
   }
   
   out_stream << endl << endl;
   
   
-  // II. Feature 1: Printing the average price per brand and per category
-  
+// II. Feature 1: Printing the average price per brand and per category
   // organize the requisite data
   map< string, pair<float, int> > averageMap;  // pairs unique string values with a pair conisisting of the sum of prices associated with that string and the number of such prices
   
@@ -98,27 +78,23 @@ int main()
   fill_averageMap(vBrand, vPrice, averageMap);
   out_stream << "Brand" << "\t" << "Average_Price" << endl;
   print_averageMap(averageMap, out_stream);
-  
   out_stream << endl << endl;
   
   // print the average price per category
   fill_averageMap(vCategory, vPrice, averageMap);
   out_stream << "Category" << "\t" << "Average_Price" << endl;
   print_averageMap(averageMap, out_stream);
-  
   out_stream << endl << endl;
   
   
-  // III. Feature 2: Printing the years and their associated SKUs
-  
+// III. Feature 2: Printing the years and their associated SKUs  
   map< string, set<string> > dataMap;
   fill_dataMap(vYear, vSKU, dataMap);
   out_stream << "List of SKUs by year" << endl;
   print_dataMap(dataMap, out_stream);
   
   
-  // IV. End of program
-  
+// IV. End of program
   out_stream.close();
 }
 
@@ -204,7 +180,7 @@ void fill_dataMap(vector<string>& names, vector<string>& data, map< string, set<
       pair< string, set<string> > pr;
       pr.first = names[i];
       
-      dataMap.insert(pr).second;
+      dataMap.insert(pr);
       dataMap.find(names[i])->second.insert(data[i]);
     }
   }
