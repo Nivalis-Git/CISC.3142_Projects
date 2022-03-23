881cfabe2d79384fd84e62f6d2411bf99ca0cf10
Nivalis-Git <s.n.shterenberg@gmail.com>
2022-03-22 22:52:55 -0400

    Initial commit of lab2 branch
    Modified files to reflect lab2 requirements
    
    Wrote a few lines of code to start

---
+++ lab2_src/calc.cpp
@@ -0,0 +1,25 @@
+/* Student Information
+  Shterenberg, Simon
+  Project 2
+*/
+
+#include "calc.h"
+
+
+enum class Operation { add, sub, mlt, div, exp, sqrt, convert_decimal};
+
+
+struct Calculator
+{
+	Operation op;
+};
+
+
+int main()
+{
+// I. 
+
+
+// . End of program
+	return 0;
+}
